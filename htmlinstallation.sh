d52e92947269d1af8a91a9ce5848291e4d844819
Gonca Atabek Uz <gonjumixproject@gmail.com>
2020-07-07 10:04:40 +0000

    html thing

---
+++ htmlinstallation.sh
@@ -0,0 +1,11 @@
+
+#!/bin/sh
+
+/usr/bin/git clone https://github.com/gonjumixproject/HTMLandPHP-Implementation-MakeMeFamous.git
+cd /HTMLandPHP-Implementation-MakeMeFamous
+cp * /var/www/html
+ip=$(hostname -I | cut -d ' ' -f 1)
+sed -i "s/\<THISWILLBETHEIPOFTHESERVER\>/$ip/g" /var/www/html/*
+cd /var/www/html
+chown www-data:www-data *
+
