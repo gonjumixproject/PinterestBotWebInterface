8963784ed7034b611497c1c28ad037dfbf5e4fd0
Gonca Atabek Uz <gonjumixproject@gmail.com>
2020-07-07 10:11:13 +0000

    HTML folder name change

--- htmlinstallation.sh
+++ htmlinstallation.sh
@@ -2,7 +2,7 @@
 #!/bin/sh
 
 /usr/bin/git clone https://github.com/gonjumixproject/HTMLandPHP-Implementation-MakeMeFamous.git
-cd /HTMLandPHP-Implementation-MakeMeFamous
+cd HTMLandPHP-Implementation-MakeMeFamous
 cp * /var/www/html
 ip=$(hostname -I | cut -d ' ' -f 1)
 sed -i "s/\<THISWILLBETHEIPOFTHESERVER\>/$ip/g" /var/www/html/*
